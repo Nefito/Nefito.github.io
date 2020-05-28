9282d5fce18a7d1ce38268c2fe7202ea93fdf78b
Nefito <vkorzhov00@gmail.com>
2020-05-28 20:09:58 +0300

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,16 +29,24 @@ class Main extends Component {
 
       const HomePage = () => {
       return(
-          <Home
-              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
-      );
-    }
+              <Home
+                  dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                  promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                  leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+              />
+            );
+      }
+
+      const DishWithId = ({match}) => {
+      return(
+              <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+      };
 
     return (
         <Switch>
+            <Route path='/menu/:dishId' component={DishWithId} />
             <Route exact path='/contactus' component={Contact} />} />
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
