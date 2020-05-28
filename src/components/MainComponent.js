a74a1dc17f9f244de1936ee489b7ef2e485f7cab
Nefito <vkorzhov00@gmail.com>
2020-05-28 19:52:38 +0300

    SPA part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -7,16 +7,39 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Contact from './ContactComponent';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
+
 
 class Main extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS
+    };
+  }
   render() {
 
       const HomePage = () => {
-          return(<Home/>);
-      }
+      return(
+          <Home
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+      );
+    }
 
     return (
         <Switch>
+            <Route exact path='/contactus' component={Contact} />} />
             <Route path='/home' component={HomePage} />
             <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
             <Redirect to="/home" />
