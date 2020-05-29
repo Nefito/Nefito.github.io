a580c99574a672bca7060c839b97178ca6df1f1d
Nefito <vkorzhov00@gmail.com>
2020-05-29 21:24:01 +0300

    Fetch from Server

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,23 +1,14 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardText,
-  CardBody,
-  CardTitle,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  Breadcrumb,
-  BreadcrumbItem,
-  Button,
-  Row,
-  Col,
-  Label
-} from "reactstrap";
+import { Card, CardImg, CardText, CardBody,
+            CardTitle, Modal, ModalHeader, ModalBody,
+            Breadcrumb, BreadcrumbItem, Button, Row,
+            Col, Label
+        } from "reactstrap";
+
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Link } from "react-router-dom";
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 
 const required = val => val && val.length;
@@ -131,7 +122,7 @@ function RenderDish({ dish }) {
   return (
     <div className="col-12 col-md-5 m-1">
         <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
+          <CardImg top src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
@@ -190,7 +181,6 @@ const DishDetailComponent = props => {
           <RenderDish dish={props.dish} />
           <RenderComments comments={props.comments}
             addComment={props.addComment}
-            postComment={props.postComment}
             dishId={props.dish.id}/>
         </div>
       </div>
